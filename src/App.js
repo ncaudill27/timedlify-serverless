002f9e13ebaf6283ff72cb4fae65174fb3c767b8
Nelson Caudill <nelson.a.caudill@gmail.com>
2020-08-14 00:15:40 -0400

    Control form elements

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
 
-  const [view, setView] = useState('new');
+  const [view, setView] = useState('main');
 
   const router = () => {
     switch(view) {
@@ -15,7 +15,7 @@ function App() {
         </>;
 
       case 'new':
-        return <Create />;
+        return <Create changeView={setView} />;
       
       default:
         break;
@@ -62,37 +62,49 @@ function Timedify({changeView}) {
   </>;
 }
 
-function Create() {
+function Create({changeView}) {
 
   const [name, setName] = useState('');
   const handleChange = e => setName(e.target.value);
+
+  const [playlist, setPlaylist] = useState('');
+  const handlePlaylist = e => setPlaylist(e.target.value);
+
+  
+  const [color, setColor] = useState('');
+  const handleColor = e => setColor(e.target.value);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    changeView('main');
+  }
   
   return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <label for='name'>Name</label>
         <input id='name' type='text' name='name' value={name} onChange={handleChange} autoComplete='off' />
 
         <h3>Select a playlist</h3>
-        <input className='playlist' type='radio' id='playlist1' name='playlist' value='playlist1' />
+        <input className='playlist' type='radio' id='playlist1' name='playlist' value='playlist1' checked={playlist === 'playlist1'} onChange={handlePlaylist} />
         <label className='playlist-for' for='playlist1'>Playlist 1</label>
-        <input className='playlist' type='radio' id='playlist2' name='playlist' value='playlist2' />
+        <input className='playlist' type='radio' id='playlist2' name='playlist' value='playlist2' checked={playlist === 'playlist2'} onChange={handlePlaylist} />
         <label className='playlist-for' for='playlist2'>Playlist 2</label>
-        <input className='playlist' type='radio' id='playlist3' name='playlist' value='playlist3' />
+        <input className='playlist' type='radio' id='playlist3' name='playlist' value='playlist3' checked={playlist === 'playlist3'} onChange={handlePlaylist} />
         <label className='playlist-for' for='playlist3'>Playlist 3</label>
-        <input className='playlist' type='radio' id='playlist4' name='playlist' value='playlist4' />
+        <input className='playlist' type='radio' id='playlist4' name='playlist' value='playlist4' checked={playlist === 'playlist4'} onChange={handlePlaylist} />
         <label className='playlist-for' for='playlist4'>Playlist 4</label>
 
         <h3>Color</h3>
         <div class='colors'>
-          <input className='color' type='radio' id='ff7300' name='color' value='ff7300' />
+          <input className='color' type='radio' id='ff7300' name='color' value='#ff7300' checked={color === '#ff7300'} onChange={handleColor} />
           <label className='color-for' for='ff7300' style={{backgroundColor: '#ff7300'}}></label>
-          <input className='color' type='radio' id='#ff0000' name='color' value='#ff0000' />
+          <input className='color' type='radio' id='#ff0000' name='color' value='#ff0000' checked={color === '#ff0000'} onChange={handleColor} />
           <label className='color-for' for='#ff0000' style={{backgroundColor: '#ff0000'}}></label>
-          <input className='color' type='radio' id='#ae00ff' name='color' value='#ae00ff' />
+          <input className='color' type='radio' id='#ae00ff' name='color' value='#ae00ff' checked={color === '#ae00ff'} onChange={handleColor} />
           <label className='color-for' for='#ae00ff' style={{backgroundColor: '#ae00ff'}}></label>
-          <input className='color' type='radio' id='#00ccff' name='color' value='#00ccff' />
+          <input className='color' type='radio' id='#00ccff' name='color' value='#00ccff' checked={color === '#00ccff'} onChange={handleColor} />
           <label className='color-for' for='#00ccff' style={{backgroundColor: '#00ccff'}}></label>
-          <input className='color' type='radio' id='#d9ff00' name='color' value='#d9ff00' />
+          <input className='color' type='radio' id='#d9ff00' name='color' value='#d9ff00' checked={color === '#d9ff00'} onChange={handleColor} />
           <label className='color-for' for='#d9ff00' style={{backgroundColor: '#d9ff00'}}></label>
         </div>
         <button type='submit'>Create Timer</button>
